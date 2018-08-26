2d0d0b222d5e822d1210f4f1b7a20e426d108cb7
Standret <pstandret@gmail.com>
2018-08-26 11:56:52 +0300

    add comment

--- CppMySQLConnector/Source.cpp
+++ CppMySQLConnector/Source.cpp
@@ -16,21 +16,25 @@ int main(int argc, char* argv[])
 {
 	try
 	{
-		SQLCustomDriver driver("tcp://127.0.0.1:3306", "root", "Zasada_19sqlroot");
+		// create connection
+		SQLCustomDriver driver("adress", "username", "password");
 		driver.use_db("labwork1_standret");
 
+		// get object (the tabe sgould be created)
 		auto res = driver.get_object<Soundtrack>("SELECT * FROM SOUNDTRACK", FieldFactory::get_soundtrack_field());
-		//int i = res[0].get_value<int>(FieldFactory::get_soundtrack_field()[0]);
-		Soundtrack ss;
-		ss.duration = 999;
-		ss.name = "999";
-		ss.owner = "999";
-		ss.publish = "1999-12-12";
-		ss.rating = 4.5;
-		std::vector<Storable*> obj{ &ss };
+
+		Soundtrack moskau("Moskau", "Dschinghis Khan", 267, 4.5, "1979-01-01"), rasputin("RASPUTIN", "The Essential Boney", 265, 5, "1978-01-01");
+
+		// set object recieve Storable object (your class must inherit from Storable and implement all virtual method)
+		std::vector<Storable*> obj{ &moskau, &rasputin };
+
+		// true - objects have been set successfully
+		// false - object is empty or other error
+		// SQL throw error (you need to catch it by yourself)
 		auto update = driver.set_object(obj);
 	}
 	catch (sql::SQLException &e) {
+		// default tracker for show error
 		std::cout << "# ERR: SQLException in " << __FILE__;
 		std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
 		std::cout << "# ERR: " << e.what();
